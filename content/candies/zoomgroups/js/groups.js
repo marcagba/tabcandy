d4b639975204343d0c047a4e1238deaa64c8b178
aza <aza@More-Better-Internet.local>
2010-03-20 11:01:45 -0700

    Original Flavour: Changed animation type from jQuery to css-transition. Doesn't seem to make that much different. Also, fixed a bug where I didn't remove the "transitionend" event handler which was causing all sorts of strange things (like zooming in on a tab when the search-box was used.

---
+++ content/candies/zoomgroups/js/groups.js
@@ -0,0 +1,388 @@
+(function(){
+
+var numCmp = function(a,b){ return a-b; }
+
+function min(list){ return list.slice().sort(numCmp)[0]; }
+function max(list){ return list.slice().sort(numCmp).reverse()[0]; }
+
+function isEventOverElement(event, el){
+  var hit = {nodeName: null};
+  var isOver = false;
+  
+  var hiddenEls = [];
+  while(hit.nodeName != "BODY" && hit.nodeName != "HTML"){
+    hit = document.elementFromPoint(event.clientX, event.clientY);
+    if( hit == el ){
+      isOver = true;
+      break;
+    }
+    $(hit).hide();
+    hiddenEls.push(hit);
+  }
+  
+  var hidden;
+  [$(hidden).show() for([,hidden] in Iterator(hiddenEls))];
+  return isOver;
+}
+
+function Group(){}
+Group.prototype = {
+  _children: [],
+  _container: null,
+  _padding: 30,
+  
+  _getBoundingBox: function(){
+    var els = this._children;
+    var el;
+    var boundingBox = {
+      top:    min( [$(el).position().top  for([,el] in Iterator(els))] ),
+      left:   min( [$(el).position().left for([,el] in Iterator(els))] ),
+      bottom: max( [$(el).position().top  for([,el] in Iterator(els))] )  + $(els[0]).height(),
+      right:  max( [$(el).position().left for([,el] in Iterator(els))] ) + $(els[0]).width(),
+    };
+    boundingBox.height = boundingBox.bottom - boundingBox.top;
+    boundingBox.width  = boundingBox.right - boundingBox.left;
+    return boundingBox;
+  },
+  
+  _getContainerBox: function(){
+    var pos = $(this._container).position();
+    var w = $(this._container).width();
+    var h = $(this._container).height();
+    return {
+      top: pos.top,
+      left: pos.left,
+      bottom: pos.top + h,
+      right: pos.left + w,
+      height: h,
+      width: w
+    }
+  },
+  
+  create: function(listOfEls){
+    this._children = $(listOfEls).toArray();
+
+    var boundingBox = this._getBoundingBox();
+    var padding = 30;
+    var container = $("<div class='group'/>")
+      .css({
+        position: "absolute",
+        top: boundingBox.top-padding,
+        left: boundingBox.left-padding,
+        width: boundingBox.width+padding*2,
+        height: boundingBox.height+padding*2,
+        zIndex: -100,
+        opacity: 0,
+      })
+      .data("group", this)
+      .appendTo("body")
+      .animate({opacity:1.0}).dequeue();
+    
+    var resizer = $("<div class='resizer'/>")
+      .css({
+        position: "absolute",
+        width: 16, height: 16,
+        bottom: 0, right: 0,
+      }).appendTo(container);
+
+    var titlebar = $("<div class='titlebar'><input class='name' value=''/><div class='close'>x</div></div>")
+      .appendTo(container)
+    
+    titlebar.css({
+        width: container.width(),
+        position: "relative",
+        top: -(titlebar.height()+2),
+        left: -1,
+      })
+
+    // On delay, show the title bar.
+    var shouldShow = false;
+    container.mouseover(function(){
+      shouldShow = true;
+      setTimeout(function(){
+        if( shouldShow == false ) return;
+        container.find("input").focus();
+        titlebar
+          .css({width: container.width()})
+          .animate({ opacity: 1}).dequeue();        
+      }, 500);
+    }).mouseout(function(e){
+      shouldShow = false;
+      if( isEventOverElement(e, container.get(0) )) return;
+      titlebar.animate({opacity:0}).dequeue();
+    })
+
+    this._container = container;
+    
+    this._addHandlers(container);
+    this._updateGroup();
+
+    var els = this._children;
+    this._children = [];
+    for(var i in els){
+      this.add( els[i] );
+    }
+  },
+  
+  add: function(el){
+    if($.inArray(el, this._children) == -1)
+      this._children.push( el );
+
+    $(el).droppable("disable");
+    
+    this._updateGroup();
+    this.arrange();
+  },
+  
+  remove: function(el){
+    $(el).data("toRemove", true);
+    this._children = this._children.filter(function(child){
+      if( $(child).data("toRemove") == true ){
+        $(child).data("group", null);
+        scaleTab( $(child), 160/$(child).width());
+        $(child).droppable("enable");        
+        return false;
+      }
+      else return true;
+    })
+    $(el).data("toRemove", false);
+    
+    if( this._children.length == 0 ){
+      this._container.fadeOut(function() $(this).remove());
+    } else {
+      this.arrange();
+    }
+    
+  },
+  
+  _updateGroup: function(){
+    var self = this;
+    this._children.forEach(function(el){
+      $(el).data("group", self);
+    });    
+  },
+  
+  arrange: function(){
+    if( this._children.length < 2 ) return;
+    var bb = this._getContainerBox();
+    var tab;
+
+    // TODO: This is an hacky heuristic. Fix this layout algorithm.
+    var pad = 10;
+    var w = parseInt(Math.sqrt(((bb.height+pad) * (bb.width+pad))/(this._children.length+4)));
+    var h = w * (2/3);
+
+    var x=pad;
+    var y=pad;
+    for([,tab] in Iterator(this._children)){      
+      scaleTab( $(tab), w/$(tab).width());
+      $(tab).animate({
+        top:y+bb.top, left:x+bb.left,
+      }, 250);
+      
+      x += w+pad;
+      if( x+w+pad > $(this._container).width()){x = pad;y += h+pad;}
+    }
+    
+  },
+  
+  _addHandlers: function(container){
+    var self = this;
+    
+    $(container).draggable({
+      start: function(){
+        $(container).data("origPosition", $(container).position());
+        self._children.forEach(function(el){
+          $(el).data("origPosition", $(el).position());
+        });
+      },
+      drag: function(e, ui){
+        var origPos = $(container).data("origPosition");
+        dX = ui.offset.left - origPos.left;
+        dY = ui.offset.top - origPos.top;
+        $(self._children).each(function(){
+          $(this).css({
+            left: $(this).data("origPosition").left + dX,
+            top:  $(this).data("origPosition").top + dY
+          })
+        })
+      }
+    });
+    
+    
+    $(container).droppable({
+      over: function(){
+        $dragged.addClass("willGroup");
+      },
+      out: function(){
+        $dragged.data("group").remove($dragged);
+        $dragged.removeClass("willGroup");
+      },
+      drop: function(){
+        $dragged.removeClass("willGroup");
+        self.add( $dragged.get(0) )
+      },
+      accept: ".tab",
+    });
+        
+    $(container).resizable({
+      handles: "se",
+      aspectRatio: true,
+      stop: function(){
+        self.arrange();
+      } 
+    })
+    
+    }
+}
+
+var zIndex = 100;
+var $dragged = null;
+var timeout = null;
+
+window.Groups = {
+  Group: Group, 
+  
+  dragOptions: {
+    start:function(){
+      $dragged = $(this);
+      $dragged.data('isDragging', true);
+    },
+    stop: function(){
+      $dragged.data('isDragging', false);
+      $(this).css({zIndex: zIndex});
+      $dragged = null;          
+      zIndex += 1;
+    },
+    zIndex: 999,
+  },
+  
+  dropOptions: {
+    accept: ".tab",
+    tolerance: "pointer",
+    greedy: true,
+    drop: function(e){
+      $target = $(e.target);
+  
+      // Only drop onto the top z-index
+      if( $target.css("zIndex") < $dragged.data("topDropZIndex") ) return;
+      $dragged.data("topDropZIndex", $target.css("zIndex") );
+      $dragged.data("topDrop", $target);
+      
+      // This strange timeout thing solves the problem of when
+      // something is dropped onto multiple potential drop targets.
+      // We wait a little bit to see get all drops, and then we have saved
+      // the top-most one and drop onto that.
+      clearTimeout( timeout );
+      var dragged = $dragged;
+      var target = $target;
+      timeout = setTimeout( function(){
+        dragged.removeClass("willGroup")   
+  
+        dragged.animate({
+          top: target.position().top+15,
+          left: target.position().left+15,      
+        }, 100);
+        
+        setTimeout( function(){
+          var group = $(target).data("group");
+          if( group == null ){
+            var group = new Group();
+            group.create( [target, dragged] );            
+          } else {
+            group.add( dragged.get(0) )
+          }
+          
+        }, 100);
+        
+        
+      }, 10 );
+      
+      
+    },
+    over: function(e){
+      $dragged.addClass("willGroup");
+      $dragged.data("topDropZIndex", 0);    
+    },
+    out: function(){      
+      $dragged.removeClass("willGroup");
+    }
+  }, 
+  
+  arrange: function() {
+    var $groups = $('.group');
+    var count = $groups.length;
+    var columns = Math.ceil(Math.sqrt(count));
+    var rows = ((columns * columns) - count >= columns ? columns - 1 : columns); 
+    var padding = 12;
+    var startX = padding;
+    var startY = 100;
+    var totalWidth = window.innerWidth - startX;
+    var totalHeight = window.innerHeight - startY;
+    var w = (totalWidth / columns) - padding;
+    var h = (totalHeight / rows) - padding;
+    var x = startX;
+    var y = startY;
+    
+    $groups.each(function(i) {
+      $(this).css({left: x, top: y, width: w, height: h});
+      
+      $(this).data('group').arrange();
+      
+      x += w + padding;
+      if(i % columns == columns - 1) {
+        x = startX;
+        y += h + padding;
+      }
+    });
+  }  
+};
+
+function scaleTab( el, factor ){  
+  var $el = $(el);
+
+  $el.animate({
+    width: $el.width()*factor,
+    height: $el.height()*factor,
+    fontSize: parseInt($el.css("fontSize"))*factor,
+  },250).dequeue();
+}
+
+
+$(".tab").data('isDragging', false)
+  .draggable(window.Groups.dragOptions)
+  .droppable(window.Groups.dropOptions);
+
+$(".tab").click(function(){
+  // ZOOM!
+
+  var ffVersion = parseFloat(navigator.userAgent.match(/\d{8}.*(\d\.\d)/)[1]);
+  if( ffVersion < 3.7 ) Utils.error("css-transitions require Firefox 3.7+");  
+  
+  var [w,h] = [$(this).width(), $(this).height()];
+  var origPos = $(this).position();
+  var scale = window.innerWidth/w;
+  var fontSize = parseInt($(this).css("font-size"));
+  
+  $(this).addClass("scale-animate").css({
+    top: 0, left: 0,
+    width:w*scale, height:h*scale,
+    fontSize: fontSize*scale
+  }).bind("transitionend", function(e){
+    // We will get one of this events for every property CSS-animated...
+    // I choose one randomly (width) and only do things for that.
+    if( e.originalEvent.propertyName != "width" ) return;
+
+    // Switch tabs, and the re-size and re-position the animated
+    // tab image.
+    var self = this;
+    setTimeout(function(){
+      $(self)
+        .removeClass("scale-animate")
+        .css({top: origPos.top, left: origPos.left, width:w, height:h, fontSize:fontSize});
+    }, 500);
+    
+  })
+})
+
+})();
---
+++ content/js/core/mirror.js
@@ -0,0 +1,289 @@
+(function(){
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cu = Components.utils;
+const Cr = Components.results;
+
+function _isIframe(doc){
+  var win = doc.defaultView;
+  return win.parent != win;
+}
+
+// ----------
+var TabCanvas = function(tab, canvas){ this.init(tab, canvas) }
+TabCanvas.prototype = {
+  init: function(tab, canvas){
+    this.tab = tab;
+    this.canvas = canvas;
+    this.window = window;
+            
+    $(canvas).data("link", this);
+
+    var w = $(canvas).width();
+    var h = $(canvas).height();
+    $(canvas).attr({width:w, height:h});
+      
+    var self = this;
+    this.paintIt = function(evt) { 
+      // note that "window" is unreliable in this context, so we'd use self.window if needed
+      self.tab.mirror.triggerPaint();
+/*       self.window.Utils.log('paint it', self.tab.url); */
+    };
+  },
+  
+  attach: function() {
+    this.tab.contentWindow.addEventListener("MozAfterPaint", this.paintIt, false);
+  },
+     
+  detach: function() {
+    this.tab.contentWindow.removeEventListener("MozAfterPaint", this.paintIt, false);
+  },
+  
+  paint: function(evt){
+    var $ = this.window.$;
+    if( $ == null ) {
+      Utils.log('null $ in paint');
+      return;
+    }
+    
+    var $canvas = $(this.canvas);
+    var ctx = this.canvas.getContext("2d");
+  
+    var w = $canvas.attr('width');
+    var h = $canvas.attr('height');
+  
+    var fromWin = this.tab.contentWindow;
+    if(fromWin == null) {
+      Utils.log('null fromWin in paint');
+      return;
+    }
+    
+    Utils.assert('chrome windows don\'t get paint (TabCanvas.paint)', 
+      fromWin.location.protocol != "chrome:");
+
+    var scaler = w/fromWin.innerWidth;
+  
+    // TODO: Potentially only redraw the dirty rect? (Is it worth it?)
+
+    ctx.save();
+    ctx.scale(scaler, scaler);
+    try{
+      ctx.drawWindow( fromWin, fromWin.scrollX, fromWin.scrollY, w/scaler, h/scaler, "#fff" );
+    } catch(e){
+      Utils.error('paint', e);   
+    }
+    
+    ctx.restore();
+  },
+  
+  animate: function(options, duration){
+    Utils.log('on animate', this.tab.contentWindow.location.href);
+
+    // TODO: This doesn't seem to scale the rest of the interface elements at the same
+    // width, leaving unfortunately long trails.
+
+    var self = this;
+    if( duration == null ) duration = 0;
+        
+    var $canvas = $(this.canvas);
+    var w = $canvas.width();
+    var h = $canvas.height();
+    
+    var newW = (w/h)*options.height;
+    var newH = options.height;
+    
+    $canvas.width(w);
+    $canvas.height(h);    
+    $canvas.animate({width:newW, height:newH}, duration, function(){
+      $canvas.attr("width", newW);
+      $canvas.attr("height", newH);
+      self.paint(null);      
+    } );
+    this.paint(null);
+  }
+}
+
+// ----------
+var TabMirror = function( ){ this.init() }
+TabMirror.prototype = {
+  init: function(){
+    var self = this;
+    
+    // When a tab is updated, update the mirror
+    Tabs.onReady( function(evt){
+      self.update(evt.tab);
+    });
+    
+    // When a tab is closed, unlink.    
+    Tabs.onClose( function(){
+      self.unlink(this);
+    });
+    
+    // For each tab, create the link.
+    Tabs.forEach(function(tab){
+      self.link(tab);
+    });    
+     
+    this.paintingPaused = 0;
+    this.heartbeatIndex = 0;  
+    this._fireNextHeartbeat();    
+  },
+  
+  _heartbeat: function() {
+    try {
+      var now = Utils.getMilliseconds();
+      var count = Tabs.length;
+      if(count && this.paintingPaused <= 0) {
+        this.heartbeatIndex++;
+        if(this.heartbeatIndex >= count)
+          this.heartbeatIndex = 0;
+          
+        var tab = Tabs[this.heartbeatIndex];
+        var mirror = tab.mirror; 
+        if(mirror) {
+          var iconUrl = tab.raw.linkedBrowser.mIconURL;
+          var label = tab.raw.label;
+          $fav = $(mirror.favEl);
+          $name = $(mirror.nameEl);
+          $canvas = $(mirror.canvasEl);
+          
+          if(iconUrl != $fav.attr("src")) { 
+            $fav.attr("src", iconUrl);
+            mirror.triggerPaint();
+          }
+            
+          if($name.text() != label) {
+            $name.text(label);
+            mirror.triggerPaint();
+          }
+          
+          if(tab.url != mirror.url) {
+            mirror.url = tab.url;
+            mirror.triggerPaint();
+          }
+          
+          var w = $canvas.width();
+          var h = $canvas.height();
+          if(w != $canvas.attr('width') || h != $canvas.attr('height')) {
+            $canvas.attr('width', w);
+            $canvas.attr('height', h);
+            mirror.triggerPaint();
+          }
+
+          if(mirror.needsPaint) {
+            mirror.tabCanvas.paint();
+            
+            if(Utils.getMilliseconds() - mirror.needsPaint > 5000)
+              mirror.needsPaint = 0;
+          }
+        }
+      }
+    } catch(e) {
+      Utils.error('heartbeat', e);
+    }
+    
+    this._fireNextHeartbeat();
+  },
+  
+  _fireNextHeartbeat: function() {
+    var self = this;
+    window.setTimeout(function() {
+      self._heartbeat();
+    }, 100);
+  },   
+    
+  _customize: function(func){
+    // pass
+    // This gets set by add-ons/extensions to MirrorTab
+  },
+  
+  _createEl: function(tab){
+    var div = $("<div class='tab'><span class='name'>&nbsp;</span><img class='fav'/><canvas class='thumb'/></div>")
+      .data("tab", tab)
+      .appendTo("body");
+      
+    if( tab.url.match("chrome:") )
+      div.hide();
+    
+    this._customize(div);
+    
+    tab.mirror = {}; 
+    tab.mirror.needsPaint = 0;
+    tab.mirror.el = div.get(0);
+    tab.mirror.favEl = $('.fav', div).get(0);
+    tab.mirror.nameEl = $('.name', div).get(0);
+    tab.mirror.canvasEl = $('.thumb', div).get(0);
+    
+    tab.mirror.triggerPaint = function() {
+    	var date = new Date();
+    	this.needsPaint = date.getTime();
+    };
+    
+    var doc = tab.contentDocument;
+    if( !_isIframe(doc) ) {
+      tab.mirror.tabCanvas = new TabCanvas(tab, tab.mirror.canvasEl);    
+      tab.mirror.tabCanvas.attach();
+      tab.mirror.triggerPaint();
+    }
+  },
+  
+  update: function(tab){
+    this.link(tab);
+
+    if(tab.mirror && tab.mirror.tabCanvas)
+      tab.mirror.triggerPaint();
+  },
+  
+  link: function(tab){
+    // Don't add duplicates
+    if(tab.mirror)
+      return false;
+    
+    // Don't do anything that starts with a chrome URL
+    if( tab.contentWindow.location.protocol == "chrome:" )
+      return false;
+    
+    // Add the tab to the page
+    this._createEl(tab);
+    return true;
+  },
+  
+  unlink: function(tab){
+    var mirror = tab.mirror;
+    if(mirror) {
+      var tabCanvas = mirror.tabCanvas;
+      if(tabCanvas)
+        tabCanvas.detach();
+      
+      $(mirror.el).remove();
+      
+      tab.mirror = null;
+    }
+  }  
+};
+
+// ----------
+window.TabMirror = {
+  _private: new TabMirror(), 
+  
+  customize: function(func) {
+    // Apply the custom handlers to all existing elements
+    // TODO: Make this modular: so that it only exists in one place.
+    //       No breaking DRY!
+    func($("div.tab"));
+    
+    // Apply it to all future elements.
+    TabMirror.prototype._customize = func;
+  },
+
+  pausePainting: function() {
+    this._private.paintingPaused++;
+  },
+  
+  resumePainting: function() {
+    this._private.paintingPaused--;
+  }
+};
+
+})();
