1c158829eec47a5cf229717dc916c0855a28f4cb
Aza Raskin <aza@mozilla.com>
2010-04-22 12:38:37 -0400

    First pass at an implementation of stacks. + In Zoomgroups put some pages into a group, then scale it down + It will animate itself into a stack + Pull it out again and it will go back to being a zoomgroup

---
+++ content/candies/revision-a/js/tabitems.js
@@ -0,0 +1,256 @@
+// ##########
+window.TabItem = function(container, tab) {
+  this._init(container);
+  this.tab = tab;
+  this.defaultSize = new Point(TabItems.tabWidth, TabItems.tabHeight);
+  this.setResizable(true);
+};
+
+window.TabItem.prototype = $.extend(new Item(), {
+  // ----------  
+  reloadBounds: function() {
+    this.bounds = Utils.getBounds(this.container);
+    this._updateDebugBounds();
+  },
+  
+  // ----------  
+  setBounds: function(rect, immediately) {
+    var $container = $(this.container);
+    var $title = $('.tab-title', $container);
+    var $thumb = $('.thumb', $container);
+    var $close = $('.close', $container);
+    var css = {};
+
+    if(rect.left != this.bounds.left)
+      css.left = rect.left;
+      
+    if(rect.top != this.bounds.top)
+      css.top = rect.top;
+      
+    if(rect.width != this.bounds.width) {
+      var widthExtra = parseInt($container.css('padding-left')) 
+          + parseInt($container.css('padding-right'));
+          
+      css.width = rect.width - widthExtra;
+      var scale = css.width / TabItems.tabWidth;
+      css.fontSize = TabItems.fontSize * scale;
+    }
+
+    if(rect.height != this.bounds.height) {
+      var heightExtra = parseInt($container.css('padding-top')) 
+          + parseInt($container.css('padding-bottom'));
+          
+      css.height = rect.height - heightExtra; 
+    }
+      
+    if($.isEmptyObject(css))
+      return;
+      
+    this.bounds.copy(rect);
+
+    if(immediately) {
+      $container.css(css);
+      
+/*
+      if(css.fontSize) {
+        if(css.fontSize < 8)
+          $title.hide();
+        else
+          $title.show();
+      }
+*/
+    } else {
+      TabMirror.pausePainting();
+      $container.animate(css, {complete: function() {
+        TabMirror.resumePainting();
+      }}).dequeue();
+    }
+
+    if(css.fontSize) {
+      if(css.fontSize < 8)
+        $title.fadeOut();
+      else
+        $title.fadeIn();
+    }
+
+    if(css.width) {
+      if(css.width < 30) {
+        $thumb.fadeOut();
+        $close.fadeOut();
+      } else {
+        $thumb.fadeIn();
+        $close.fadeIn();
+      }
+    }
+
+    this._updateDebugBounds();
+  },
+
+  // ----------
+  setZ: function(value) {
+    $(this.container).css({zIndex: value});
+  },
+    
+  // ----------
+  close: function() {
+    this.tab.close();
+  },
+  
+  // ----------
+  addOnClose: function(referenceObject, callback) {
+    this.tab.mirror.addOnClose(referenceObject, callback);      
+  },
+
+  // ----------
+  removeOnClose: function(referenceObject) {
+    this.tab.mirror.removeOnClose(referenceObject);      
+  },
+  
+  // ----------  
+  setResizable: function(value){
+    var self = this;
+    
+    var $resizer = $('.expander', this.container);
+    if(value) {
+      $resizer.fadeIn();
+      $(this.container).resizable({
+        handles: "se",
+        aspectRatio: true,
+        minWidth: TabItems.minTabWidth,
+        minHeight: TabItems.minTabWidth * (TabItems.tabHeight / TabItems.tabWidth),
+        resize: function(){
+          self.reloadBounds();
+        },
+        stop: function(){
+          self.reloadBounds();
+          self.pushAway();
+        } 
+      });
+    } else {
+      $resizer.fadeOut();
+      $(this.container).resizable('destroy');
+    }
+  }
+});
+
+// ##########
+window.TabItems = {
+  minTabWidth: 40, 
+  tabWidth: 160,
+  tabHeight: 120, 
+  fontSize: 9,
+
+  init: function() {
+    var self = this;
+    
+    
+    function mod($div){
+      if(window.Groups) {        
+        $div.data('isDragging', false);
+        $div.draggable(window.Groups.dragOptions);
+        $div.droppable(window.Groups.dropOptions);
+      }
+      
+      $div.mousedown(function(e) {
+        if(!Utils.isRightClick(e))
+          self.lastMouseDownTarget = e.target;
+      });
+        
+      $div.mouseup(function(e) { 
+        var same = (e.target == self.lastMouseDownTarget);
+        self.lastMouseDownTarget = null;
+        if(!same)
+          return;
+        
+        if(e.target.className == "close") {
+          $(this).find("canvas").data("link").tab.close(); }
+        else {
+          if(!$(this).data('isDragging')) {
+
+            // ZOOM! 
+            var orig = {
+              width: $(this).width(),
+              height:  $(this).height(),
+              pos: $(this).position()
+            }
+
+            var scale = window.innerWidth/orig.width;
+            
+            var tab = Tabs.tab(this);
+            var mirror = tab.mirror;
+            
+            var overflow = $("body").css("overflow");
+            $("body").css("overflow", "hidden");
+            
+            function onZoomDone(){
+              TabMirror.resumePainting();
+              $(this).find("canvas").data("link").tab.focus();
+              $(this).css({
+                top:   orig.pos.top,
+                left:  orig.pos.left,
+                width: orig.width,
+                height:orig.height,
+                })
+                .removeClass("front");  
+              Navbar.show();    
+              $("body").css("overflow", overflow);              
+            }
+  
+            TabMirror.pausePainting();
+            $(this)
+              .addClass("front")
+              .animate({
+                top:    -10,
+                left:   0,
+                easing: "easein",
+                width:  orig.width*scale,
+                height: orig.height*scale
+                }, 200, onZoomDone);
+            
+          } else {
+            $(this).find("canvas").data("link").tab.raw.pos = $(this).position();
+          }
+        }
+      });
+      
+      $("<div class='close'></div>").appendTo($div);
+      $("<div class='expander'></div>").appendTo($div);
+  
+      var items = [];
+      $div.each(function() {
+        var tab = Tabs.tab(this);
+        var item = new TabItem(this, tab);
+        $(this).data('tabItem', item);    
+        items.push(item); 
+      });
+      
+      if($div.length == 1)
+        Groups.newTab($div.data('tabItem'));
+      else {
+        var box = Items.getPageBounds();
+        box.inset(20, 20);
+        
+        Items.arrange(items, box, {padding: 10, animate:false});
+      }
+      
+      // TODO: Figure out this really weird bug?
+      // Why is that:
+      //    $div.find("canvas").data("link").tab.url
+      // returns chrome://tabcandy/content/candies/original/index.html for
+      // every $div (which isn't right), but that
+      //   $div.bind("test", function(){
+      //      var url = $(this).find("canvas").data("link").tab.url;
+      //   });
+      //   $div.trigger("test")
+      // returns the right result (i.e., the per-tab URL)?
+      // I'm so confused...
+      // Although I can use the trigger trick, I was thinking about
+      // adding code in here which sorted the tabs into groups.
+      // -- Aza
+    }
+    
+    window.TabMirror.customize(mod);
+  }
+};
+
+TabItems.init();
---
+++ content/candies/revision-a/js/ui.js
@@ -0,0 +1,311 @@
+(function(){
+
+// ##########
+Navbar = {
+  get el(){
+    var win = Utils.activeWindow;
+    var navbar = win.gBrowser.ownerDocument.getElementById("navigator-toolbox");
+    return navbar;      
+  },
+  show: function(){ this.el.collapsed = false; },
+  hide: function(){ this.el.collapsed = true;}
+}
+
+// ##########
+var Tabbar = {
+  get el(){ return window.Tabs[0].raw.parentNode; },
+  height: window.Tabs[0].raw.parentNode.getBoundingClientRect().height,
+  hide: function(){
+    var self = this;
+    $(self.el).animate({"marginTop":-self.height}, 150, function(){
+      self.el.collapsed = true;
+    });
+  },
+  show: function(){
+    this.el.collapsed = false;
+    $(this.el).animate({"marginTop":0}, 150);
+  },
+  get isHidden(){ return this.el.collapsed; }
+}
+
+// ##########
+window.Page = {
+  startX: 30, 
+  startY: 70,
+    
+  // ----------  
+  init: function() {    
+    Utils.homeTab.raw.maxWidth = 60;
+    Utils.homeTab.raw.minWidth = 60;
+    
+    Tabs.onClose(function(){
+      Utils.homeTab.focus();
+      Toolbar.unread = 0;    
+      return false;
+    });
+    
+    Tabs.onOpen(function(){
+      setTimeout(function(){
+        Toolbar.unread += 1;
+      },100);
+    });
+    
+    var lastTab = null;
+    Tabs.onFocus(function(){
+      // If we switched to TabCandy window...
+      if( this.contentWindow == window && lastTab != null && lastTab.mirror != null){
+        Toolbar.unread = 0;
+        // If there was a lastTab we want to animate
+        // its mirror for the zoom out.
+        var $tab = $(lastTab.mirror.el);
+        
+        var [w,h, pos, z] = [$tab.width(), $tab.height(), $tab.position(), $tab.css("zIndex")];
+        var scale = window.innerWidth / w;
+
+        var overflow = $("body").css("overflow");
+        $("body").css("overflow", "hidden");
+        
+        var mirror = lastTab.mirror;
+        TabMirror.pausePainting();
+        $tab.css({
+            top: 0, left: 0,
+            width: window.innerWidth,
+            height: h * (window.innerWidth/w),
+            zIndex: 999999,
+        }).animate({
+            top: pos.top, left: pos.left,
+            width: w, height: h
+        },250, '', function() {
+          $tab.css("zIndex",z);
+          $("body").css("overflow", overflow);
+          TabMirror.resumePainting();
+        });
+      }
+      lastTab = this;
+    });
+    
+    $("#tabbar-toggle").click(function(){
+      if( Tabbar.isHidden ){
+        Tabbar.show();
+        $(this).removeClass("tabbar-off");        
+      }
+      else {
+        Tabbar.hide();
+        $(this).addClass("tabbar-off");        
+      }
+    });
+  },
+  
+  // ----------  
+  findOpenSpaceFor: function($div) {
+    var w = window.innerWidth;
+    var h = 0;
+    var bufferX = 30;
+    var bufferY = 30;
+    var rects = [];
+    var r;
+    var $el;
+    $(".tab:visible").each(function(i) {
+      if(this == $div.get(0))
+        return;
+        
+      $el = $(this);
+      r = {x: parseInt($el.css('left')),
+        y: parseInt($el.css('top')),
+        w: parseInt($el.css('width')) + bufferX,
+        h: parseInt($el.css('height')) + bufferY};
+        
+      if(r.x + r.w > w)
+        w = r.x + r.w;
+
+      if(r.y + r.h > h)
+        h = r.y + r.h;
+  
+      rects.push(r);
+    });
+
+    if(!h) 
+      return { 'x': this.startX, 'y': this.startY };
+      
+    var canvas = document.createElement('canvas');
+    $(canvas).attr({width:w,height:h});
+
+    var ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'rgb(255, 255, 255)';
+    ctx.fillRect(0, 0, w, h);
+    ctx.fillStyle = 'rgb(0, 0, 0)';
+    var count = rects.length;
+    var a;
+    for(a = 0; a < count; a++) {
+      r = rects[a];
+      ctx.fillRect(r.x, r.y, r.w, r.h);
+    }
+    
+    var divWidth = parseInt($div.css('width')) + bufferX;
+    var divHeight = parseInt($div.css('height')) + bufferY;
+    var strideX = divWidth / 4;
+    var strideY = divHeight / 4;
+    var data = ctx.getImageData(0, 0, w, h).data;
+
+    function isEmpty(x1, y1) {
+      return (x1 >= 0 && y1 >= 0
+          && x1 < w && y1 < h
+          && data[(y1 * w + x1) * 4]);
+    }
+
+    function isEmptyBox(x1, y1) {
+      return (isEmpty(x1, y1) 
+          && isEmpty(x1 + (divWidth - 1), y1)
+          && isEmpty(x1, y1 + (divHeight - 1))
+          && isEmpty(x1 + (divWidth - 1), y1 + (divHeight - 1)));
+    }
+    
+    for(var y = this.startY; y < h; y += strideY) {
+      for(var x = this.startX; x < w; x += strideX) {
+        if(isEmptyBox(x, y)) {
+          for(; y > this.startY + 1; y--) {
+            if(!isEmptyBox(x, y - 1))
+              break;
+          }
+
+          for(; x > this.startX + 1; x--) {
+            if(!isEmptyBox(x - 1, y))
+              break;
+          }
+
+          return { 'x': x, 'y': y };
+        }
+  	  }
+    }
+
+    return { 'x': this.startX, 'y': h };        
+  }
+}
+
+// ##########
+function ArrangeClass(name, func){ this.init(name, func); };
+ArrangeClass.prototype = {
+  init: function(name, func){
+    this.$el = this._create(name);
+    this.arrange = func;
+    if(func) this.$el.click(func);
+  },
+  
+  _create: function(name){
+    return $("<a class='action' href='#'/>").text(name).css({margin:5}).appendTo("#actions");
+  }
+}
+
+// ##########
+function UIClass(){ 
+  this.navBar = Navbar;
+  this.tabBar = Tabbar;
+  this.devMode = false;
+  
+  var self = this;
+  
+  // ___ URL Params
+  var params = document.location.search.replace('?', '').split('&');
+  $.each(params, function(index, param) {
+    var parts = param.split('=');
+    if(parts[0] == 'dev' && parts[1] == '1') 
+      self.devMode = true;
+  });
+  
+  // ___ Dev Mode
+  if(this.devMode) {
+    Switch.insert('body', '');
+    $('<br><br>').appendTo("#actions");
+    this._addArrangements();
+  }
+  
+  // ___ Navbar
+  this.navBar.hide();
+  
+  Tabs.onFocus(function() {
+    if(this.contentWindow.location.host == "tabcandy")
+      self.navBar.hide();
+    else
+      self.navBar.show();
+  });
+
+  Tabs.onOpen(function(a, b) {
+    self.navBar.show();
+  });
+
+  // ___ Finish up
+  Page.init();
+};
+
+// ----------
+UIClass.prototype = {
+  _addArrangements: function() {
+    this.grid = new ArrangeClass("Grid", function(value) {
+      if(typeof(Groups) != 'undefined')
+        Groups.removeAll();
+    
+      var immediately = false;
+      if(typeof(value) == 'boolean')
+        immediately = value;
+    
+      var box = new Rect(Page.startX, Page.startY, TabItems.tabWidth, TabItems.tabHeight); 
+      $(".tab:visible").each(function(i){
+        var item = Items.item(this);
+        item.setBounds(box, immediately);
+        
+        box.left += box.width + 30;
+        if( box.left > window.innerWidth - (box.width + Page.startX)){ // includes allowance for the box shadow
+          box.left = Page.startX;
+          box.top += box.height + 30;
+        }
+      });
+    });
+        
+    this.site = new ArrangeClass("Site", function() {
+      Groups.removeAll();
+      
+      var groups = [];
+      $(".tab:visible").each(function(i) {
+        $el = $(this);
+        var tab = Tabs.tab(this);
+        
+        var url = tab.url; 
+        var domain = url.split('/')[2]; 
+        var domainParts = domain.split('.');
+        var mainDomain = domainParts[domainParts.length - 2];
+        if(groups[mainDomain]) 
+          groups[mainDomain].push($(this));
+        else 
+          groups[mainDomain] = [$(this)];
+      });
+      
+      var createOptions = {dontPush: true, dontArrange: true};
+      
+      var leftovers = [];
+      for(key in groups) {
+        var set = groups[key];
+        if(set.length > 1) {
+          group = new Group(set, createOptions);
+        } else
+          leftovers.push(set[0]);
+      }
+      
+    /*   if(leftovers.length > 1) { */
+        group = new Group(leftovers, createOptions);
+    /*   } */
+      
+      Groups.arrange();
+    });
+  },
+  
+  // ----------
+  newTab: function(url, inBackground) {
+    Tabs.open(url, inBackground);
+  }
+};
+
+// ----------
+window.UI = new UIClass();
+
+})();
+
