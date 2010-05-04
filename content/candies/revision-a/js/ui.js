04d1b60a4f0e9909349ee50be94633e815a89f17
Aza Raskin <aza@mozilla.com>
2010-05-04 15:57:19 -0700

    With Ian's help this commit: + Adds an new-tab animation that Feels Right(tm) [although the timing might still need tweaking] + When new tabs are added while TabCandy is focused, the no longer animate in from a random location.

---
+++ content/candies/revision-a/js/ui.js
@@ -0,0 +1,559 @@
+(function(){
+
+// ##########
+Navbar = {
+  // ----------
+  get el(){
+    var win = Utils.activeWindow;
+    if(win) {
+      var navbar = win.gBrowser.ownerDocument.getElementById("navigator-toolbox");
+      return navbar;      
+    }
+
+    return null;
+  },
+  
+  get urlBar(){
+    var win = Utils.activeWindow;
+    if(win) {
+      var navbar = win.gBrowser.ownerDocument.getElementById("urlbar");
+      return navbar;      
+    }
+
+    return null;    
+  },
+
+  // ----------
+  show: function() {
+    var el = this.el;
+    if(el)
+      el.collapsed = false; 
+    else { // needs a little longer to get going
+      var self = this;
+      setTimeout(function() {
+        self.show();
+      }, 300); 
+    }
+  },
+
+  // ----------
+  hide: function() {
+    var el = this.el;
+    if(el)
+      el.collapsed = true; 
+    else { // needs a little longer to get going
+      var self = this;
+      setTimeout(function() {
+        self.hide();
+      }, 300); 
+    }
+  },
+}
+
+// ##########
+var Tabbar = {
+  // ----------
+  // Variable: _hidden
+  // We keep track of whether the tabs are hidden in this (internal) variable
+  // so we still have access to that information during the window's unload event,
+  // when window.Tabs no longer exists.
+  _hidden: false, 
+  get el(){ return window.Tabs[0].raw.parentNode; },
+  height: window.Tabs[0].raw.parentNode.getBoundingClientRect().height,
+  hide: function(animate) {
+    var self = this;
+    this._hidden = true;
+    
+    if( animate == false ) speed = 0;
+    else speed = 150;
+    
+    $(self.el).animate({"marginTop":-self.height}, speed, function(){
+      self.el.collapsed = true;
+    });
+  },
+  show: function(animate) {
+    this._hidden = false;
+
+    if( animate == false ) speed = 0;
+    else speed = 150;
+        
+    this.el.collapsed = false;
+    $(this.el).animate({"marginTop":0}, speed);
+  },
+  
+  showOnlyTheseTabs: function(tabs){
+    var visibleTabs = [];
+    // UI.tabBar.el.children is not a real array and does contain
+    // useful functions like filter or forEach. Convert it into a real array.
+    var tabBarTabs = [];
+    for( var i=0; i<UI.tabBar.el.children.length; i++ ){
+      tabBarTabs.push(UI.tabBar.el.children[i]);
+    }
+    
+    for each( var tab in tabs ){
+      var rawTab = tab.tab.raw;
+      var toShow = tabBarTabs.filter(function(testTab){
+        return testTab == rawTab;
+      }); 
+      visibleTabs = visibleTabs.concat( toShow );
+    }
+
+    tabBarTabs.forEach(function(tab){
+      tab.collapsed = true;
+    });
+    
+    // Show all of the tabs in the group and move them (in order)
+    // that they appear in the group to the end of the tab strip.
+    // This way the tab order is matched up to the group's thumbnail
+    // order.
+    visibleTabs.forEach(function(tab){
+      tab.collapsed = false;
+      Utils.activeWindow.gBrowser.moveTabTo(tab, UI.tabBar.el.children.length-1);
+    });
+    
+  },
+  get isHidden(){ return this._hidden; }
+}
+
+// ##########
+window.Page = {
+  startX: 30, 
+  startY: 70,
+  
+  show: function(){
+    Utils.homeTab.focus();
+    UI.tabBar.hide(false);
+  },
+    
+  // ----------  
+  init: function() {    
+    Utils.homeTab.raw.maxWidth = 60;
+    Utils.homeTab.raw.minWidth = 60;
+        
+    Tabs.onClose(function(){
+      // Only go back to the TabCandy tab when there you close the last
+      // tab of a group.
+      var group = Groups.getActiveGroup();
+      if( group && group._children.length == 0 )
+        Page.show();
+      return false;
+    });
+        
+    var lastTab = null;
+    Tabs.onFocus(function(){
+      // If we switched to TabCandy window...
+      if( this.contentWindow == window && lastTab != null && lastTab.mirror != null){
+        Groups.getActiveGroup().reorderBasedOnTabOrder();        
+
+        UI.tabBar.hide(false);
+        // If there was a lastTab we want to animate
+        // its mirror for the zoom out.
+        // Zoom out!
+        var $tab = $(lastTab.mirror.el);
+        
+        var [w,h, pos, z] = [$tab.width(), $tab.height(), $tab.position(), $tab.css("zIndex")];
+        var scale = window.innerWidth / w;
+
+        var overflow = $("body").css("overflow");
+        $("body").css("overflow", "hidden");
+        
+        var mirror = lastTab.mirror;
+        TabMirror.pausePainting();
+        $tab.css({
+            top: 0, left: 0,
+            width: window.innerWidth,
+            height: h * (window.innerWidth/w),
+            zIndex: 999999,
+        }).animate({
+            top: pos.top, left: pos.left,
+            width: w, height: h
+        },350, '', function() {
+          $tab.css("zIndex",z);
+          $("body").css("overflow", overflow);
+          window.Groups.setActiveGroup(null);
+          TabMirror.resumePainting();
+        });
+      }
+      lastTab = this;
+    });
+  },
+  
+  // ----------  
+  findOpenSpaceFor: function($div) {
+    var w = window.innerWidth;
+    var h = 0;
+    var bufferX = 30;
+    var bufferY = 30;
+    var rects = [];
+    var r;
+    var $el;
+    $(".tab:visible").each(function(i) {
+      if(this == $div.get(0))
+        return;
+        
+      $el = $(this);
+      r = {x: parseInt($el.css('left')),
+        y: parseInt($el.css('top')),
+        w: parseInt($el.css('width')) + bufferX,
+        h: parseInt($el.css('height')) + bufferY};
+        
+      if(r.x + r.w > w)
+        w = r.x + r.w;
+
+      if(r.y + r.h > h)
+        h = r.y + r.h;
+  
+      rects.push(r);
+    });
+
+    if(!h) 
+      return { 'x': this.startX, 'y': this.startY };
+      
+    var canvas = document.createElement('canvas');
+    $(canvas).attr({width:w,height:h});
+
+    var ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'rgb(255, 255, 255)';
+    ctx.fillRect(0, 0, w, h);
+    ctx.fillStyle = 'rgb(0, 0, 0)';
+    var count = rects.length;
+    var a;
+    for(a = 0; a < count; a++) {
+      r = rects[a];
+      ctx.fillRect(r.x, r.y, r.w, r.h);
+    }
+    
+    var divWidth = parseInt($div.css('width')) + bufferX;
+    var divHeight = parseInt($div.css('height')) + bufferY;
+    var strideX = divWidth / 4;
+    var strideY = divHeight / 4;
+    var data = ctx.getImageData(0, 0, w, h).data;
+
+    function isEmpty(x1, y1) {
+      return (x1 >= 0 && y1 >= 0
+          && x1 < w && y1 < h
+          && data[(y1 * w + x1) * 4]);
+    }
+
+    function isEmptyBox(x1, y1) {
+      return (isEmpty(x1, y1) 
+          && isEmpty(x1 + (divWidth - 1), y1)
+          && isEmpty(x1, y1 + (divHeight - 1))
+          && isEmpty(x1 + (divWidth - 1), y1 + (divHeight - 1)));
+    }
+    
+    for(var y = this.startY; y < h; y += strideY) {
+      for(var x = this.startX; x < w; x += strideX) {
+        if(isEmptyBox(x, y)) {
+          for(; y > this.startY + 1; y--) {
+            if(!isEmptyBox(x, y - 1))
+              break;
+          }
+
+          for(; x > this.startX + 1; x--) {
+            if(!isEmptyBox(x - 1, y))
+              break;
+          }
+
+          return { 'x': x, 'y': y };
+        }
+  	  }
+    }
+
+    return { 'x': this.startX, 'y': h };        
+  }
+}
+
+// ##########
+function ArrangeClass(name, func){ this.init(name, func); };
+ArrangeClass.prototype = {
+  init: function(name, func){
+    this.$el = this._create(name);
+    this.arrange = func;
+    if(func) this.$el.click(func);
+  },
+  
+  _create: function(name){
+    return $("<a class='action' href='#'/>").text(name).css({margin:5}).appendTo("#actions");
+  }
+}
+
+// ##########
+function UIClass(){ 
+  this.navBar = Navbar;
+  this.tabBar = Tabbar;
+  this.devMode = false;
+  
+  var self = this;
+  
+  // ___ URL Params
+  var params = document.location.search.replace('?', '').split('&');
+  $.each(params, function(index, param) {
+    var parts = param.split('=');
+    if(parts[0] == 'dev' && parts[1] == '1') 
+      self.devMode = true;
+  });
+  
+  // ___ Dev Mode
+  if(this.devMode) {
+    Switch.insert('body', '');
+    $('<br><br>').appendTo("#actions");
+    this._addArrangements();
+  }
+  
+  // ___ Navbar
+  this.navBar.hide();
+  
+  Tabs.onFocus(function() {
+    if(this.contentWindow.location.host == "tabcandy")
+      self.navBar.hide();
+    else
+      self.navBar.show();
+  });
+
+  Tabs.onOpen(function(a, b) {
+    self.navBar.show();
+  });
+
+  // ___ tab bar
+  this.$tabBarToggle = $("#tabbar-toggle")
+    .click(function() {
+      if(self.tabBar.isHidden)
+        self.showTabBar();
+      else
+        self.hideTabBar();
+    });
+
+  // ___ Page
+  Page.init();
+  
+  // ___ Storage
+  var data = Storage.read();
+  this.storageSanity(data);
+  if(data.hideTabBar)
+    this.hideTabBar();
+    
+  if(data.dataVersion < 2) {
+    data.groups = null;
+    data.tabs = null;
+  }
+      
+  Groups.reconstitute(data.groups);
+  TabItems.reconstitute(data.tabs);
+  
+  $(window).bind('beforeunload', function() {
+    if(self.initialized) {
+      var data = {
+        dataVersion: 2,
+        hideTabBar: self.tabBar._hidden,
+        groups: Groups.getStorageData(),
+        tabs: TabItems.getStorageData(), 
+        pageBounds: Items.getPageBounds()
+      };
+      
+      Storage.write(data);
+    }
+  });
+  
+  // ___ resizing
+  if(data.pageBounds) {
+    this.pageBounds = data.pageBounds;
+    this.resize();
+  } else 
+    this.pageBounds = Items.getPageBounds();    
+  
+  $(window).resize(function() {
+    self.resize();
+  });
+  
+  // ___ Dev Menu
+  this.addDevMenu();
+  
+  // ___ Done
+  this.initialized = true;
+};
+
+// ----------
+UIClass.prototype = {
+  // ----------
+  showTabBar: function() {
+    this.tabBar.show();
+    this.$tabBarToggle.removeClass("tabbar-off");        
+  },
+
+  // ----------
+  hideTabBar: function() {
+    this.tabBar.hide();
+    this.$tabBarToggle.addClass("tabbar-off");        
+  },
+
+  // ----------
+  resize: function() {
+/*     Groups.repositionNewTabGroup(); */
+    
+    var items = Items.getTopLevelItems();
+    var itemBounds = new Rect(this.pageBounds);
+    itemBounds.width = 1;
+    itemBounds.height = 1;
+    $.each(items, function(index, item) {
+      if(item.locked)
+        return;
+        
+      var bounds = item.getBounds();
+      itemBounds = (itemBounds ? itemBounds.union(bounds) : new Rect(bounds));
+    });
+
+    var oldPageBounds = new Rect(this.pageBounds);
+    
+    var newPageBounds = Items.getPageBounds();
+    if(newPageBounds.width < this.pageBounds.width && newPageBounds.width > itemBounds.width)
+      newPageBounds.width = this.pageBounds.width;
+
+    if(newPageBounds.height < this.pageBounds.height && newPageBounds.height > itemBounds.height)
+      newPageBounds.height = this.pageBounds.height;
+
+    var wScale;
+    var hScale;
+    if(Math.abs(newPageBounds.width - this.pageBounds.width)
+        > Math.abs(newPageBounds.height - this.pageBounds.height)) {
+      wScale = newPageBounds.width / this.pageBounds.width;
+      hScale = newPageBounds.height / itemBounds.height;
+    } else {
+      wScale = newPageBounds.width / itemBounds.width;
+      hScale = newPageBounds.height / this.pageBounds.height;
+    }
+    
+    var scale = Math.min(hScale, wScale);
+    var self = this;
+    $.each(items, function(index, item) {
+      if(item.locked)
+        return;
+        
+      var bounds = item.getBounds();
+
+      bounds.left += newPageBounds.left - self.pageBounds.left;
+      bounds.left *= scale;
+      bounds.width *= scale;
+
+      bounds.top += newPageBounds.top - self.pageBounds.top;            
+      bounds.top *= scale;
+      bounds.height *= scale;
+      
+      item.setBounds(bounds, true);
+    });
+    
+    this.pageBounds = Items.getPageBounds();
+  },
+  
+  // ----------
+  addDevMenu: function() {
+    var html = '<select style="position:absolute; top:5px;">'; 
+    var $select = $(html)
+      .appendTo('body')
+      .change(function () {
+        var index = $(this).val();
+        commands[index].code();
+      });
+      
+    var commands = [{
+      name: '*', 
+      code: function() {
+      }
+    }, {
+      name: 'home', 
+      code: function() {
+        location.href = '../../index.html';
+      }
+    }];
+      
+    var count = commands.length;
+    var a;
+    for(a = 0; a < count; a++) {
+      html = '<option value="'
+        + a
+        + '">'
+        + commands[a].name
+        + '</option>';
+        
+      $select.append(html);
+    }
+  },
+
+  // ----------
+  storageSanity: function(data) {
+    if(data) {
+      // TODO: cleanliness check
+    }
+  },
+  
+  // ----------
+  _addArrangements: function() {
+    this.grid = new ArrangeClass("Grid", function(value) {
+      if(typeof(Groups) != 'undefined')
+        Groups.removeAll();
+    
+      var immediately = false;
+      if(typeof(value) == 'boolean')
+        immediately = value;
+    
+      var box = new Rect(Page.startX, Page.startY, TabItems.tabWidth, TabItems.tabHeight); 
+      $(".tab:visible").each(function(i){
+        var item = Items.item(this);
+        item.setBounds(box, immediately);
+        
+        box.left += box.width + 30;
+        if( box.left > window.innerWidth - (box.width + Page.startX)){ // includes allowance for the box shadow
+          box.left = Page.startX;
+          box.top += box.height + 30;
+        }
+      });
+    });
+        
+    this.site = new ArrangeClass("Site", function() {
+      Groups.removeAll();
+      
+      var groups = [];
+      $(".tab:visible").each(function(i) {
+        $el = $(this);
+        var tab = Tabs.tab(this);
+        
+        var url = tab.url; 
+        var domain = url.split('/')[2]; 
+        var domainParts = domain.split('.');
+        var mainDomain = domainParts[domainParts.length - 2];
+        if(groups[mainDomain]) 
+          groups[mainDomain].push($(this));
+        else 
+          groups[mainDomain] = [$(this)];
+      });
+      
+      var createOptions = {dontPush: true, dontArrange: true};
+      
+      var leftovers = [];
+      for(key in groups) {
+        var set = groups[key];
+        if(set.length > 1) {
+          group = new Group(set, createOptions);
+        } else
+          leftovers.push(set[0]);
+      }
+      
+    /*   if(leftovers.length > 1) { */
+        group = new Group(leftovers, createOptions);
+    /*   } */
+      
+      Groups.arrange();
+    });
+  },
+  
+  // ----------
+  newTab: function(url, inBackground) {
+    Tabs.open(url, inBackground);
+  }
+};
+
+// ----------
+window.UI = new UIClass();
+
+})();
+
---
+++ content/js/core/mirror.js
@@ -0,0 +1,367 @@
+// Title: mirror.js
+(function(){
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cu = Components.utils;
+const Cr = Components.results;
+
+function _isIframe(doc){
+  var win = doc.defaultView;
+  return win.parent != win;
+}
+
+// ----------
+var TabCanvas = function(tab, canvas){ this.init(tab, canvas) }
+TabCanvas.prototype = {
+  init: function(tab, canvas){
+    this.tab = tab;
+    this.canvas = canvas;
+    this.window = window;
+            
+    $(canvas).data("link", this);
+
+    var w = $(canvas).width();
+    var h = $(canvas).height();
+    $(canvas).attr({width:w, height:h});
+      
+    var self = this;
+    this.paintIt = function(evt) { 
+      // note that "window" is unreliable in this context, so we'd use self.window if needed
+      self.tab.mirror.triggerPaint();
+/*       self.window.Utils.log('paint it', self.tab.url); */
+    };
+  },
+  
+  attach: function() {
+    this.tab.contentWindow.addEventListener("MozAfterPaint", this.paintIt, false);
+  },
+     
+  detach: function() {
+    try {
+      this.tab.contentWindow.removeEventListener("MozAfterPaint", this.paintIt, false);
+    } catch(e) {
+      // ignore
+    }
+  },
+  
+  paint: function(evt){
+    var $ = this.window.$;
+    if( $ == null ) {
+      Utils.log('null $ in paint');
+      return;
+    }
+    
+    var $canvas = $(this.canvas);
+    var ctx = this.canvas.getContext("2d");
+  
+    var w = $canvas.attr('width');
+    var h = $canvas.attr('height');
+    if(!w || !h)
+      return;
+  
+    var fromWin = this.tab.contentWindow;
+    if(fromWin == null) {
+      Utils.log('null fromWin in paint');
+      return;
+    }
+    
+    var scaler = w/fromWin.innerWidth;
+  
+    // TODO: Potentially only redraw the dirty rect? (Is it worth it?)
+
+    ctx.save();
+    ctx.scale(scaler, scaler);
+    try{
+      ctx.drawWindow( fromWin, fromWin.scrollX, fromWin.scrollY, w/scaler, h/scaler, "#fff" );
+    } catch(e){
+      Utils.error('paint', e);   
+    }
+    
+    ctx.restore();
+  },
+  
+  animate: function(options, duration){
+    Utils.log('on animate', this.tab.contentWindow.location.href);
+
+    // TODO: This doesn't seem to scale the rest of the interface elements at the same
+    // width, leaving unfortunately long trails.
+
+    var self = this;
+    if( duration == null ) duration = 0;
+        
+    var $canvas = $(this.canvas);
+    var w = $canvas.width();
+    var h = $canvas.height();
+    
+    var newW = (w/h)*options.height;
+    var newH = options.height;
+    
+    $canvas.width(w);
+    $canvas.height(h);    
+    $canvas.animate({width:newW, height:newH}, duration, function(){
+      $canvas.attr("width", newW);
+      $canvas.attr("height", newH);
+      self.paint(null);      
+    } );
+    this.paint(null);
+  }
+}
+
+// ##########
+// Class: Mirror
+// A single tab in the browser and its visual representation in the tab candy window. 
+// Note that it implements the <Subscribable> interface.
+function Mirror(tab, manager) {
+  this.tab = tab;
+  this.manager = manager;
+  
+  var html = "<div class='tab'>" +
+              "<div class='favicon'><img/></div>" +
+              "<div class='thumb'><div class='thumbShadow'></div><canvas/></div>" +
+              "<span class='tab-title'>&nbsp;</span>" +              
+             "</div>";
+             
+  
+  var div = $(html)
+    .data("tab", this.tab)
+    .appendTo("body");
+      
+  this.needsPaint = 0;
+  this.canvasSizeForced = false;
+  this.el = div.get(0);
+  this.favEl = $('.favicon>img', div).get(0);
+  this.nameEl = $('.tab-title', div).get(0);
+  this.canvasEl = $('.thumb canvas', div).get(0);
+      
+  var doc = this.tab.contentDocument;
+  if( !_isIframe(doc) ) {
+    this.tabCanvas = new TabCanvas(this.tab, this.canvasEl);    
+    this.tabCanvas.attach();
+    this.triggerPaint();
+  }
+  
+  this.tab.mirror = this;
+  this.manager._customize(div);
+}
+
+Mirror.prototype = $.extend(new Subscribable(), {  
+  // ----------
+  // Function: triggerPaint
+  // Forces the mirror in question to update its thumbnail. 
+  triggerPaint: function() {
+  	var date = new Date();
+  	this.needsPaint = date.getTime();
+  },
+  
+  // ----------
+  // Function: foreceCanvasSize
+  // Repaints the thumbnail with the given resolution, and forces it 
+  // to stay that resolution until unforceCanvasSize is called. 
+  forceCanvasSize: function(w, h) {
+    this.canvasSizeForced = true;
+    var $canvas = $(this.canvasEl);
+    $canvas.attr('width', w);
+    $canvas.attr('height', h);
+    this.tabCanvas.paint();
+  },
+  
+  // ----------
+  // Function: unforceCanvasSize
+  // Stops holding the thumbnail resolution; allows it to shift to the
+  // size of thumbnail on screen. Note that this call does not nest, unlike
+  // <TabMirror.resumePainting>; if you call forceCanvasSize multiple 
+  // times, you just need a single unforce to clear them all. 
+  unforceCanvasSize: function() {
+    this.canvasSizeForced = false;
+  }
+});
+
+// ##########
+// Class: TabMirror
+// A singleton that manages all of the <Mirror>s in the system.
+var TabMirror = function( ){ this.init() }
+TabMirror.prototype = {
+  // ----------
+  // Function: init
+  // Set up the necessary tracking to maintain the <Mirror>s.
+  init: function(){
+    var self = this;
+    
+/*
+    // When a tab is opened, create the mirror
+    Tabs.onOpen(function() {
+      self.update(this);
+    });
+*/
+
+    // When a tab is updated, update the mirror
+    Tabs.onReady( function(evt){
+      self.update(evt.tab);
+    });
+    
+    // When a tab is closed, unlink.    
+    Tabs.onClose( function(){
+      self.unlink(this);
+    });
+    
+    // For each tab, create the link.
+    Tabs.forEach(function(tab){
+      self.link(tab);
+    });    
+     
+    this.paintingPaused = 0;
+    this.heartbeatIndex = 0;  
+    this._fireNextHeartbeat();    
+  },
+  
+  _heartbeat: function() {
+    try {
+      var now = Utils.getMilliseconds();
+      var count = Tabs.length;
+      if(count && this.paintingPaused <= 0) {
+        this.heartbeatIndex++;
+        if(this.heartbeatIndex >= count)
+          this.heartbeatIndex = 0;
+          
+        var tab = Tabs[this.heartbeatIndex];
+        var mirror = tab.mirror; 
+        if(mirror) {
+          var iconUrl = tab.raw.linkedBrowser.mIconURL;
+          var label = tab.raw.label;
+          $fav = $(mirror.favEl);
+          $name = $(mirror.nameEl);
+          $canvas = $(mirror.canvasEl);
+          
+          if(iconUrl != $fav.attr("src")) { 
+            $fav.attr("src", iconUrl);
+            mirror.triggerPaint();
+          }
+            
+          if($name.text() != label) {
+            $name.text(label);
+            mirror.triggerPaint();
+          }
+          
+          if(tab.url != mirror.url) {
+            var oldURL = mirror.url;
+            mirror.url = tab.url;
+            mirror._sendToSubscribers('urlChanged', {oldURL: oldURL, newURL: tab.url});
+            mirror.triggerPaint();
+          }
+          
+          if(!mirror.canvasSizeForced) {
+            var w = $canvas.width();
+            var h = $canvas.height();
+            if(w != $canvas.attr('width') || h != $canvas.attr('height')) {
+              $canvas.attr('width', w);
+              $canvas.attr('height', h);
+              mirror.triggerPaint();
+            }
+          }
+          
+          if(mirror.needsPaint) {
+            mirror.tabCanvas.paint();
+            
+            if(Utils.getMilliseconds() - mirror.needsPaint > 5000)
+              mirror.needsPaint = 0;
+          }
+        }
+      }
+    } catch(e) {
+      Utils.error('heartbeat', e);
+    }
+    
+    this._fireNextHeartbeat();
+  },
+  
+  _fireNextHeartbeat: function() {
+    var self = this;
+    window.setTimeout(function() {
+      self._heartbeat();
+    }, 100);
+  },   
+    
+  _customize: function(func){
+    // pass
+    // This gets set by add-ons/extensions to MirrorTab
+  },
+  
+  _createEl: function(tab){
+    new Mirror(tab, this); // sets tab.mirror to itself
+  },
+  
+  update: function(tab){
+    this.link(tab);
+
+    if(tab.mirror && tab.mirror.tabCanvas)
+      tab.mirror.triggerPaint();
+  },
+  
+  link: function(tab){
+    // Don't add duplicates
+    if(tab.mirror)
+      return false;
+    
+    // Add the tab to the page
+    this._createEl(tab);
+    return true;
+  },
+  
+  unlink: function(tab){
+    var mirror = tab.mirror;
+    if(mirror) {
+      mirror._sendOnClose();
+      var tabCanvas = mirror.tabCanvas;
+      if(tabCanvas)
+        tabCanvas.detach();
+      
+      $(mirror.el).remove();
+      
+      tab.mirror = null;
+    }
+  }  
+};
+
+// ----------
+window.TabMirror = {
+  _private: new TabMirror(), 
+  
+  // Function: customize
+  // Allows you to customize the tab representations as they are created. 
+  // 
+  // Parameters: 
+  //   func - a callback function that will be called every time a new 
+  //     tab or tabs are created. func should take in one parameter, a 
+  //     jQuery object representing the div enclosing the tab in question. 
+  //     This jQuery object may be singular or multiple, depending on 
+  //     the number of tabs being created. 
+  customize: function(func) {
+    // Apply the custom handlers to all existing elements
+    // TODO: Make this modular: so that it only exists in one place.
+    //       No breaking DRY!
+    func($("div.tab"));
+    
+    // Apply it to all future elements.
+    TabMirror.prototype._customize = func;
+  },
+
+  // Function: pausePainting
+  // Tells the TabMirror to stop updating thumbnails (so you can do
+  // animations without thumbnail paints causing stutters). 
+  // pausePainting can be called multiple times, but every call to 
+  // pausePainting needs to be mirrored with a call to <resumePainting>. 
+  pausePainting: function() {
+    this._private.paintingPaused++;
+  },
+  
+  // Function: resumePainting
+  // Undoes a call to <pausePainting>. For instance, if you called 
+  // pausePainting three times in a row, you'll need to call resumePainting
+  // three times before the TabMirror will start updating thumbnails again. 
+  resumePainting: function() {
+    this._private.paintingPaused--;
+  }
+};
+
+})();
